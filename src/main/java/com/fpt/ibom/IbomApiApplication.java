e8baad129e53a0432371b3aa5b50b94854b64c0b
Pham Tat Dat <datphamtat19@gmail.com>
2026-08-14 12:53:31 +0700

    initialize Spring Boot API repo

---
+++ src/main/java/com/fpt/ibom/IbomApiApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.ibom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbomApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/ibom/IbomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.ibom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
